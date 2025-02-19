8bdb08c494cd10369bead1b749f0a1f37c1b5d8e
revelationsz <rhinoam@gmail.com>
2025-02-19 18:16:42 -0500

    deployment additions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/lineup-website', // Replace 'your-repo-name' with your GitHub repository name
+  images: {
+    unoptimized: true,
+  },/* config options here */
 };
 
 export default nextConfig;
