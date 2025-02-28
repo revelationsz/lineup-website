f372a1ab9cec01c30bdb3c090e5afc78da71e693
revelationsz <rhinoam@gmail.com>
2025-02-28 16:13:51 -0500

    updated deployment

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/lineup-website', // Replace 'your-repo-name' with your GitHub repository name
+  basePath: '/lineup-website',
   images: {
     unoptimized: true,
-  },/* config options here */
+  },
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  },
 };
 
 export default nextConfig;
